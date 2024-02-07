1a105ca7f5a33818930b5e7bc4fff164b376d96f
Ricardo Ponce <ricardo.ponce@nubai.com.cv>
2024-02-07 17:14:12 -0100

    Adicionado boton de login

---
+++ public/theme/nubai-theme/js/main.js
@@ -0,0 +1,9 @@
+function open_menu_modal() {
+    
+    document.getElementById('menu-modal').style.display = 'block';
+}
+
+function close_menu_modal() {
+    
+    document.getElementById('menu-modal').style.display = 'none';
+}
