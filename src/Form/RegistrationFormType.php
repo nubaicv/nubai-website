24db80cc45848aab77270dff7a46bdbf98e90da9
Ricardo Ponce <ricardo.ponce@nubai.com.cv>
2024-04-19 20:14:00 -0100

    Formularios de login e registo estilizados com W3.CSS

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Customer;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -17,8 +18,11 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', TextType::class, [
+                'attr' => ['class' => 'w3-input w3-padding-16 w3-section']
+            ])
             ->add('agreeTerms', CheckboxType::class, [
+                'attr' => ['class' => 'w3-padding-16 w3-section'],
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
@@ -30,7 +34,7 @@ class RegistrationFormType extends AbstractType
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'attr' => ['autocomplete' => 'new-password', 'class' => 'w3-input w3-padding-16 w3-section'],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'please.enter.a.password',
