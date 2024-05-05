7964b43cc43b21ecc9a187d50651d40b8b92d166
Ricardo Ponce <ricardo.ponce@nubai.com.cv>
2024-05-05 14:11:54 -0100

    Funcionalidades do Reset Password completo. Melhorias por vir

--- src/Controller/CustomerPasswordResetController.php
+++ src/Controller/CustomerPasswordResetController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use App\Entity\Customer;
 use App\Form\ChangePasswordFormType;
 use App\Form\ResetPasswordRequestFormType;
-use App\Controller\Helper\CustomerPasswordResetHelper;
+use App\Helper\CustomerPasswordResetHelper;
 use Bolt\Controller\TwigAwareController;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
@@ -55,43 +55,65 @@ class CustomerPasswordResetController extends TwigAwareController {
         return $this->render('@theme/security/check_reset_email.twig', [
         ]);
     }
-    
-    #[Route('{_locale}/reset/{token}', name: 'reset_token_nubai', methods: ['GET'])]
+
+    #[Route('{_locale}/reset/{token}', name: 'reset_token_nubai', methods: ['GET', 'POST'])]
     public function reset(Request $request, CustomerPasswordResetHelper $resetPasswordHelper, UserPasswordHasherInterface $passwordHasher, TranslatorInterface $translator, string $token = null): Response {
-        
+
         if ($token) {
-            
+
             // We store the token in session and remove it from the URL, to avoid the URL being
             // loaded in a browser and potentially leaking the token to 3rd party JavaScript.
             $request->getSession()->set('resetToken', $token);
             return $this->redirectToRoute('reset_token_nubai');
         }
-        
+
         $token = $request->getSession()->get('resetToken');
-        
+
         if (null === $token) {
-            
+
             $this->addFlash('error', 'no.reset.password.token.found');
             return $this->redirectToRoute('reset_password_nubai');
         }
-        
+
         //Verificar aqui a igualdade dos dois tokens
-        if (!$resetPasswordHelper->validateTokenAndFetchUser($token)) {
-            
-            $this->addFlash('error', 'Error validating token');
+        try {
+
+            if (!$user = $resetPasswordHelper->validateTokenAndFetchUser($token)) {
+
+                $this->addFlash('error', 'no.reset.password.token.found');
+                return $this->redirectToRoute('reset_password_nubai');
+            }
+        } catch (\Exception $ex) {
+
+            $this->addFlash('error', $ex->getMessage());
+            return $this->redirectToRoute('reset_password_nubai');
         }
-        
+
+
         // The token is valid; allow the user to change their password.
         $form = $this->createForm(ChangePasswordFormType::class);
         $form->handleRequest($request);
-        
+
         if ($form->isSubmitted() && $form->isValid()) {
 
+            $resetPasswordHelper->removeResetRequest($token);
+
+            // Encode(hash) the plain password, and set it.
+            $encodedPassword = $passwordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+            );
+
+            $user->setPassword($encodedPassword);
+            $this->em->flush();
+
+            $request->getSession()->remove('resetToken');
+            $this->addFlash('success', 'password.changed');
             return $this->redirectToRoute('login_nubai');
         }
 
         return $this->render('@theme/security/reset_password.twig', [
-            'resetForm' => $form->createView(),
+                    'resetForm' => $form->createView(),
         ]);
     }
 
@@ -105,12 +127,16 @@ class CustomerPasswordResetController extends TwigAwareController {
         if (!$user) {
             return $this->redirectToRoute('check_reset_email_nubai');
         }
-        
+
         try {
-            
+
             $resetToken = $resetPasswordHelper->generateResetToken($user);
         } catch (\Exception $ex) {
-            
+
+            //isto aqui nao e ideal porque expoe se email existe no BD.
+            //Em production enviamos um email para admin e reencaminhamos para
+            //route 'check_reset_email_nubai' ou enviamos um email para customers cadastrados
+            //silenciosamente explicando o erro.
             $this->addFlash('error', $ex->getMessage());
             return $this->redirectToRoute('reset_password_nubai');
         }
@@ -121,12 +147,11 @@ class CustomerPasswordResetController extends TwigAwareController {
                 ->subject($translator->trans('your.password.reset.request'))
                 ->htmlTemplate('@theme/security/email/confirmation_reset.twig')
                 ->context([
-                    'resetToken' => $resetToken
+            'resetToken' => $resetToken
                 ])
         ;
-        
-        $mailer->send($email);
 
+//        $mailer->send($email);
         //redirect when user found
         return $this->redirectToRoute('check_reset_email_nubai');
     }
