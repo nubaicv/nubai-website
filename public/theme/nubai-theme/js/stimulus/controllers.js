517e8b2f466cd054a17bb5cae248a501b830809d
Ricardo Ponce <ricardo.ponce@nubai.com.cv>
2024-06-11 12:07:12 -0100

    Antes de enviar para testing

--- public/theme/nubai-theme/js/stimulus/controllers.js
+++ public/theme/nubai-theme/js/stimulus/controllers.js
@@ -6,13 +6,11 @@ Stimulus.register("mainmenu", class extends Controller {
     static targets = [ "modal" ];
 
     open() {
-//        this.modalTarget.style.display = 'block';
         document.getElementById('menu-modal').style.display = 'block';
     }
 
     close() {
-        this.modalTarget.style.display = 'none';
-//        document.getElementById('menu-modal').style.display = 'none';
+        document.getElementById('menu-modal').style.display = 'none';
     }
 });
 
@@ -77,113 +75,5 @@ Stimulus.register("cropper", class extends Controller {
     closeModal() {
 
         this.modalTarget.style.display = 'none';
-    }
-});
-
-
-
-
-
-// Content-loader controller
-Stimulus.register("content-loader", class extends Controller {
-
-    static values = {url: String, refreshInterval: Number}
-
-    connect() {
-        this.load();
-
-        if (this.hasRefreshIntervalValue) {
-            this.startRefreshing();
-        }
-    }
-
-    load() {
-        fetch(this.urlValue)
-                .then(response => response.text())
-                .then(html => this.element.innerHTML = html);
-    }
-
-    startRefreshing() {
-        this.refreshTimer = setInterval(() => {
-            this.load();
-        }, this.refreshIntervalValue);
-    }
-
-    stopRefreshing() {
-        if (this.refreshTimer) {
-            clearInterval(this.refreshTimer);
-        }
-    }
-
-    disconnect() {
-        this.stopRefreshing();
-    }
-
-});
-
-
-// Slideshow controller
-Stimulus.register("slideshow", class extends Controller {
-
-    static targets = ["slide"]
-    static values = {index: {type: Number, default: 2}}
-
-    previous() {
-        this.indexValue--;
-    }
-
-    next() {
-        this.indexValue++;
-    }
-
-    indexValueChanged() {
-        this.showCurrentSlide();
-    }
-
-    showCurrentSlide() {
-        this.slideTargets.forEach((element, index) =>
-            element.hidden = index !== this.indexValue);
-    }
-});
-
-// Greet controller
-Stimulus.register("greet", class extends Controller {
-    static targets = ["name"]
-
-    greet() {
-        console.log(`Hello, ${this.name}!`);
-    }
-
-    get name() {
-        return this.nameTarget.value;
-    }
-});
-
-// Clipboard controller
-Stimulus.register("clipboard", class extends Controller {
-    static targets = ["source"]
-    static classes = ["supported"]
-
-    copy() {
-        navigator.clipboard.writeText(this.sourceTarget.value);
-    }
-
-    connect() {
-        if ("clipboard" in navigator) {
-            this.element.classList.add(this.supportedClass);
-        }
-    }
-});
-
-// Selector controller
-Stimulus.register("selector", class extends Controller {
-    static targets = ["selectfield", "output"]
-
-    change() {
-        this.outputTarget.innerHTML = this.selectfield;
-    }
-
-    get selectfield() {
-        return this.selectfieldTarget.value;
     }
 });
--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -11,10 +11,10 @@ class ProfileController extends TwigAwareController {
     #[Route('/{_locale}/my-profile', name: 'profile_nubai', methods: ['GET', 'POST'])]
     public function index(): Response {
         
-//        if (!$this->isGranted('ROLE_USER')) {
-//
-//            return $this->redirectToRoute('login_nubai');
-//        }
+        if (!$this->isGranted('ROLE_USER')) {
+
+            return $this->redirectToRoute('login_nubai');
+        }
         
         return $this->render('@theme/profile.twig', [
             'controller_name' => 'ProfileController',
